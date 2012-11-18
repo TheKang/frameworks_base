f5c35eaa9d9b1c7ce117b00a6b4e61b9405a5048
JBirdVegas <jbirdvegas@gmail.com>
2012-11-18 07:23:30 -0600

    BootMessage: show more info in boot dialog
    Tell the users what package we are running dexOpt on currently
    
    Carbon change id: I665ab3847ec1846e40915a1fae21c43e2e7ccf6d
    
    Fix derp
    
    Conflicts:
    	services/java/com/android/server/pm/PackageManagerService.java
    
    Change-Id: Idb7e3beef14c4df7e68e03ce5fe78b10bf864337

--- services/java/com/android/server/pm/PackageManagerService.java
+++ services/java/com/android/server/pm/PackageManagerService.java
@@ -35,6 +35,7 @@ import com.android.internal.app.IMediaContainerService;
 import com.android.internal.app.ResolverActivity;
 import com.android.internal.content.NativeLibraryHelper;
 import com.android.internal.content.PackageHelper;
+import com.android.internal.policy.impl.PhoneWindowManager;
 import com.android.internal.util.FastPrintWriter;
 import com.android.internal.util.FastXmlSerializer;
 import com.android.internal.util.XmlUtils;
@@ -129,6 +130,7 @@ import android.util.SparseArray;
 import android.util.Xml;
 import android.view.Display;
 import android.view.WindowManager;
+import android.view.WindowManagerPolicy;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
@@ -540,6 +542,9 @@ public class PackageManagerService extends IPackageManager.Stub {
 
     // Stores a list of users whose package restrictions file needs to be updated
     private HashSet<Integer> mDirtyUsers = new HashSet<Integer>();
+
+    WindowManager mWindowManager;
+    private final WindowManagerPolicy mPolicy; // to set packageName
 
     final private DefaultContainerConnection mDefContainerConn =
             new DefaultContainerConnection();
@@ -1135,8 +1140,9 @@ public class PackageManagerService extends IPackageManager.Stub {
 
         mInstaller = installer;
 
-        WindowManager wm = (WindowManager)context.getSystemService(Context.WINDOW_SERVICE);
-        Display d = wm.getDefaultDisplay();
+        mWindowManager = (WindowManager)context.getSystemService(Context.WINDOW_SERVICE);
+        Display d = mWindowManager.getDefaultDisplay();
+        mPolicy = new PhoneWindowManager();
         d.getMetrics(mMetrics);
 
         File frameworkDir;
@@ -3927,7 +3933,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                             public void run() {
                                 if (!isFirstBoot()) {
                                     i[0]++;
-                                    postBootMessageUpdate(i[0], pkgsSize);
+                                    postBootMessageUpdate(i[0], pkgsSize, p.packageName);
                                 }
                                 performDexOptLI(p, false, false, true);
                             }
@@ -3935,7 +3941,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                     } else {
                         if (!isFirstBoot()) {
                             i[0]++;
-                            postBootMessageUpdate(i[0], pkgsSize);
+                            postBootMessageUpdate(i[0], pkgsSize, p.packageName);
                         }
                     }
                 }
@@ -3951,8 +3957,16 @@ public class PackageManagerService extends IPackageManager.Stub {
         }
     }
 
-    private void postBootMessageUpdate(int n, int total) {
+    private void postBootMessageUpdate(int n, int total, String packageName) {
         try {
+            // give the packagename to the PhoneWindowManager
+            ApplicationInfo ai;
+            try {
+                ai = mContext.getPackageManager().getApplicationInfo(packageName, 0);
+            } catch (Exception e) {
+                ai = null;
+            }
+            mPolicy.setPackageName((String) (ai != null ? mContext.getPackageManager().getApplicationLabel(ai) : packageName));
             ActivityManagerNative.getDefault().showBootMessage(
                     mContext.getResources().getString(
                             com.android.internal.R.string.android_upgrading_apk,
