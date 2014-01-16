34bf9070a61006b513f91eaa11709dd5f544d01c
David96 <xd_e-mail@gmx.de>
2014-01-16 16:16:48 +0530

    Base: Lockscreen blur [1/2]
    Show the content behind lockscreen, blurred.
    
    Settings: https://gerrit.omnirom.org/#/c/4702/
    
    Change-Id: I0d7ee20d6b415567f0051d716ba0db2e55511e91

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4260,6 +4260,12 @@ public final class Settings {
         public static final String BATTERY_ARC_LOCKSCREEN_HANDLE = "battery_arc_lockscreen_handle";
 
         /**
+         * Allows to show the background activity back the lockscreen
+         * @hide
+         */
+        public static final String LOCKSCREEN_SEE_THROUGH = "lockscreen_see_through";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
