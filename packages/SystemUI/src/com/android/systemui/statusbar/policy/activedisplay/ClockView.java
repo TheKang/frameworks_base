aa00a86baba52e32727121de4d0346ed2cb3cdaf
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2014-02-24 22:18:26 +0700

    [1/2] Base: fix ActiveDisplay for WifiOnly Devices
    fix http://jira.omnirom.org/browse/OMNI-637
    also fix for devices with unsupported Proximity sensor and Light sensor
    
    Change-Id: I346d4ed0ca02c6f8a6da1f3bf1de59ac9179d7b7
    
    Conflicts:
    	core/java/com/android/internal/util/slim/DeviceUtils.java
    	packages/SystemUI/res/layout-sw600dp/active_display_content.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ClockView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ClockView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ClockView.java
@@ -99,7 +99,7 @@ public class ClockView extends RelativeLayout {
         }
     };
 
-    static class AmPm {
+    private static class AmPm {
         private TextView mAmPmTextView;
         private String mAmString, mPmString;
 
@@ -155,14 +155,13 @@ public class ClockView extends RelativeLayout {
     /**
      * Class used to listen for changes to active display date/time settings
      */
-    class SettingsObserver extends ContentObserver {
+    private class SettingsObserver extends ContentObserver {
         SettingsObserver(Handler handler) {
             super(handler);
         }
 
         void observe() {
-            ContentResolver resolver =
-                    ClockView.this.mContext.getContentResolver();
+            final ContentResolver resolver = mContext.getContentResolver();
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.ACTIVE_DISPLAY_SHOW_AMPM), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
@@ -171,8 +170,7 @@ public class ClockView extends RelativeLayout {
         }
 
         void unobserve() {
-            ClockView.this.mContext.getContentResolver()
-                    .unregisterContentObserver(this);
+            mContext.getContentResolver().unregisterContentObserver(this);
         }
 
         @Override
@@ -181,13 +179,13 @@ public class ClockView extends RelativeLayout {
         }
 
         public void update() {
-            ContentResolver resolver =
-                    ClockView.this.mContext.getContentResolver();
-
-            boolean showAmPm = Settings.System.getInt(
-                    resolver, Settings.System.ACTIVE_DISPLAY_SHOW_AMPM, 0) == 1;
-            boolean showDate = Settings.System.getInt(
-                    resolver, Settings.System.ACTIVE_DISPLAY_SHOW_DATE, 0) == 1;
+            final ContentResolver resolver = mContext.getContentResolver();
+            showAmPm = (Settings.System.getIntForUser(
+                    resolver, Settings.System.ACTIVE_DISPLAY_SHOW_AMPM, 0,
+                    UserHandle.USER_CURRENT_OR_SELF) == 1);
+            boolean showDate = (Settings.System.getIntForUser(
+                    resolver, Settings.System.ACTIVE_DISPLAY_SHOW_DATE, 0,
+                    UserHandle.USER_CURRENT_OR_SELF) == 1);
 
             mAmPm.setShowAmPm(showAmPm);
             mDateView.setVisibility(showDate ? View.VISIBLE : View.INVISIBLE);
@@ -282,7 +280,7 @@ public class ClockView extends RelativeLayout {
     }
 
     private void setDateFormat() {
-        mFormat = android.text.format.DateFormat.is24HourFormat(getContext()) ? M24 : M12;
-        mAmPm.setShowAmPm(mFormat.equals(M12));
+        mFormat = DateFormat.is24HourFormat(mContext) ? M24 : M12;
+        mAmPm.setShowAmPm(mFormat.equals(M12) && showAmPm);
     }
 }
