7966a750b3d4fbbc470e68954b4471ba7efa7983
Jmz <jmztaylor@gmail.com>
2014-03-09 08:28:00 +0100

    Base: Allow disabling of FC dialogs [1/2]
    Change-Id: If87aa74ebb45849f6e5fe3a53ce9beeaac28f624
    
    k

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4080,6 +4080,12 @@ public final class Settings {
         public static final String SCREEN_ON_NOTIFICATION_LED = "screen_on_notification_led";
 
         /**
+         * Disable FC Notifications
+         * @hide
+         */
+        public static final String DISABLE_FC_NOTIFICATIONS = "disable_fc_notifications";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
