898a94a6935f977cdbef434380fac5e322807675
Schubi <schubi@erlangen.ccc.de>
2014-06-17 09:44:15 +0200

    HOVER: Disabling HOVER isn't necessary at all [1/2] …
    as we can just hide the HOVER-Button in StatusBar
    
    Change-Id: Idde34acc859e6191a71239616b2b69f09650ef49

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4328,13 +4328,6 @@ public final class Settings {
         public static final String ON_THE_GO_SERVICE_RESTART = "on_the_go_service_restart";
 
         /**
-         * Enable Hover, default is 0 (off).
-         *
-         * @hide
-         */
-        public static final String HOVER_ENABLED = "hover_enabled";
-
-        /**
          * Hover is active, default is 0 (off).
          * 0 = disabled
          * 1 = enabled
