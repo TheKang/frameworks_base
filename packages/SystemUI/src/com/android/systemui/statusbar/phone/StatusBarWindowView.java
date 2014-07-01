56a38ffa67869818b11c0adc1745850b2fd7123a
Adnan <adnan@cyngn.com>
2014-07-01 15:44:31 -0700

    BaseStatusBar: Dismiss heads up on backpress.
    Change-Id: I5b6191657cf0b74d825eb653a6e82c33e0527aad

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarWindowView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarWindowView.java
@@ -86,6 +86,7 @@ public class StatusBarWindowView extends FrameLayout
         case KeyEvent.KEYCODE_BACK:
             if (!down) {
                 mService.animateCollapsePanels();
+                mService.hideHeadsUp();
             }
             return true;
         }
