7218788e07dee3cf3bf39a01fc4e1c643cdebd18
Schubi <schubi@erlangen.ccc.de>
2014-07-29 02:10:16 +0200

    Be sure to shutdown the proximity sensor after display wake
    Change-Id: I1768f13cb48c3e056d0e137642690fc3d650c4ab

--- services/java/com/android/server/power/PowerManagerService.java
+++ services/java/com/android/server/power/PowerManagerService.java
@@ -176,7 +176,7 @@ public final class PowerManagerService extends IPowerManager.Stub
     private static final int MAX_CPU_BOOST_TIME = 5000000;
 
     // Max time allowed for proximity check
-    private static final int MAX_PROXIMITY_WAIT = 200;
+    private static final int MAX_PROXIMITY_WAIT = 750;
 
     private static final float PROXIMITY_NEAR_THRESHOLD = 5.0f;
 
@@ -1090,6 +1090,10 @@ public final class PowerManagerService extends IPowerManager.Stub
                 if (!mHandler.hasMessages(MSG_WAKE_UP)) {
                     // The sensor took too long to return and
                     // the wake event already triggered.
+                    Slog.w(TAG, "The proximity sensor took too long, wake event already triggered!");
+                    if (mSensorManager != null) {
+                        mSensorManager.unregisterListener(this);
+                    }
                     return;
                 }
                 mHandler.removeMessages(MSG_WAKE_UP);
