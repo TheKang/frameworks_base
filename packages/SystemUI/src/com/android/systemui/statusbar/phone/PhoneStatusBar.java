efe99422d21d810b8041bfd49e4f0301c925082b
kaiyiz <kaiyiz@codeaurora.org>
2014-10-15 10:20:02 +0800

    SystemUI: Add sound item in quick settings panel
    Add sound item in quick settings panel.
    
    CRs-Fixed: 715258
    
    Change-Id: I090929a3622101edc5413afe4f7c2d1863139d2c

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -993,10 +993,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         if (mQSPanel != null) {
             final QSTileHost qsh = new QSTileHost(mContext, this,
                     mBluetoothController, mLocationController, mRotationLockController,
-                    mNetworkController, mZenModeController, mHotspotController,
-                    mCastController, mFlashlightController,
-                    mUserSwitcherController, mKeyguardMonitor,
-                    mSecurityController);
+                    mNetworkController, mZenModeController, mVolumeComponent,
+                    mHotspotController, mCastController, mFlashlightController,
+                    mUserSwitcherController, mKeyguardMonitor, mSecurityController);
             mQSPanel.setHost(qsh);
             mQSPanel.setTiles(qsh.getTiles());
             mBrightnessMirrorController = new BrightnessMirrorController(mStatusBarWindow);
