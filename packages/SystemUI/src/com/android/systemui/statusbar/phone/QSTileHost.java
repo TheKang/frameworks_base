6eada480df3af4213d888ca83b4d5e49e147a28f
Schubi <schubi@erlangen.ccc.de>
2015-01-10 17:50:57 +0100

    Fix Build: Remove ApnTile, RoamingTile & DdsTile
    Change-Id: I8e30645aacf83ba9a7ef9e5cbf8363eaf35db10a

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -33,18 +33,15 @@ import android.util.Log;
 import com.android.systemui.R;
 import com.android.systemui.qs.QSTile;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
-import com.android.systemui.qs.tiles.ApnTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
 import com.android.systemui.qs.tiles.CastTile;
 import com.android.systemui.qs.tiles.CellularTile;
 import com.android.systemui.qs.tiles.ColorInversionTile;
 import com.android.systemui.qs.tiles.DataTile;
-import com.android.systemui.qs.tiles.DdsTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
-import com.android.systemui.qs.tiles.RoamingTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.settings.CurrentUserTracker;
@@ -262,12 +259,6 @@ public class QSTileHost implements QSTile.Host {
         else if (tileSpec.equals("data")
                 && mConnectivityManager.isNetworkSupported(ConnectivityManager.TYPE_MOBILE))
             return new DataTile(this);
-        else if (tileSpec.equals("roaming")) return new RoamingTile(this);
-        else if (tileSpec.equals("dds") && mTelephonyManager.isMultiSimEnabled()
-                && (mTelephonyManager.getMultiSimConfiguration()
-                == TelephonyManager.MultiSimVariants.DSDA))
-            return new DdsTile(this);
-        else if (tileSpec.equals("apn")) return new ApnTile(this);
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
     }
